7e74c2950d21db290ceceb69fdabe0f9adf58044
Xutsuro1 <96018385+Xutsuro1@users.noreply.github.com>
2023-12-21 02:27:45 +0400

    Create contacts.php

---
+++ contacts.php
@@ -0,0 +1,63 @@
+<?php
+// Connect to  MySQL database
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch data from the 'contacts' table
+$sql = "SELECT id, name, email, message FROM contacts";
+$result = $conn->query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Information</title>
+   
+</head>
+<body>
+    <div class="container">
+        <h1>Contact Information</h1>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id"] . "</td>";
+                        echo "<td>" . $row["name"] . "</td>";
+                        echo "<td>" . $row["email"] . "</td>";
+                        echo "<td>" . $row["message"] . "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='4'>No data found</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+
+<?php
+// Close the database connection
+$conn->close();
+?>
