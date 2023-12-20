e1b5263b053e76f0583bff7c4f99a2922ece1e4c
Xutsuro1 <96018385+Xutsuro1@users.noreply.github.com>
2023-12-21 02:53:52 +0400

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -29,7 +29,7 @@ $result = $conn->query($sql);
         <table border="1">
             <thead>
                 <tr>
-                    <th>ID</th>
+                    
                     <th>Name</th>
                     <th>Email</th>
                     <th>Message</th>
@@ -40,7 +40,7 @@ $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
                         echo "<tr>";
-                        echo "<td>" . $row["id"] . "</td>";
+                       
                         echo "<td>" . $row["name"] . "</td>";
                         echo "<td>" . $row["email"] . "</td>";
                         echo "<td>" . $row["message"] . "</td>";
