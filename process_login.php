89eb38ae6dc5a10aa1c0ab9ae4aaffa2701046ed
Xutsuro1 <96018385+Xutsuro1@users.noreply.github.com>
2023-12-21 05:59:18 +0400

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -3,13 +3,13 @@ class User {
     public $name;
     public $email;
     public $password;
-    public $phone;
+    public $number;
 
-    public function __construct($name, $email, $password, $phone) {
+    public function __construct($name, $email, $password, $number) {
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
-        $this->phone = $phone;
+        $this->number = $number;
     }
 }
 
@@ -17,19 +17,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $phone = $_POST['phone'];
+    $phone = $_POST['phone']; 
 
     $user = new User($name, $email, $password, $phone);
 
-    // For demonstration, let's echo the submitted data:
+    // prints
     echo "<h2>Thank you for registering!</h2>";
     echo "<p>Name: $user->name</p>";
     echo "<p>Email: $user->email</p>";
     echo "<p>Password: $user->password</p>";
-    echo "<p>Phone: $user->phone</p>";
+    echo "<p>Phone: $user->number</p>"; 
 } else {
     // If the form is not submitted properly, redirect to the login page
     header("Location: login.html");
     exit();
 }
+
+
 ?>
